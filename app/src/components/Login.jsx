2fe4fd44355f2d1ca230e2c92958dc6f90b2a9f0
krlwbstr <93937576+krlwshu@users.noreply.github.com>
2022-03-24 05:33:45 +0000

    Login page video

--- app/src/components/Login.jsx
+++ app/src/components/Login.jsx
@@ -14,6 +14,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import useNavigate from "react-router-dom";
+import { LoginWrapper } from "./uiComponents/styled/Login-styles";
 
 async function loginUser(credentials) {
   return fetch("http://localhost:5000/login", {
@@ -42,111 +43,132 @@ export default function Login({ setToken }) {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: "url(/images/ims_stock_adobe.jpeg)",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          {/* <video
-            style={{
-              backgroundSize: "cover",
-              height: "100%",
-              backgroundPosition: "center",
-              transform: "scale(1.2)",
-              overflow: "none",
-            }}
-            autoPlay
-            muted
-            loop
-            id="myVideo"
-          >
-            <source src="/video/DJI_0005.mp4" type="video/mp4" />
-            Your browser does not support HTML5 video.
-          </video> */}
-        </Grid>
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
+    <LoginWrapper>
+      <video
+        className="vid"
+        style={{
+          backgroundSize: "cover",
+          width: "100vw",
+          backgroundPosition: "left",
+          transform: "scale(1.4)",
+          overflow: "hidden",
+        }}
+        autoPlay
+        muted
+        loop
+        id="myVideo"
+      >
+        <source src="/video/DJI_0005.mp4" type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video>
+      {/* <div className="box">
+        <div className="center">
+          <h1>Inventory Management System</h1>
+          <p></p>
+          <button id="myBtn" onclick="myFunction()">
+            Pause
+          </button>
+        </div>
+      </div> */}
+      <div className="overlay"></div>
+      <div className="overlay2">
+        <Grid container component="main" sx={{ height: "100vh", px: "30%" }}>
+          <CssBaseline />
+
+          <Grid item xs={12} sm={8} md={12} elevation={6} square>
             <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 1 }}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
             >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                onChange={(e) => setUserName(e.target.value)}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+              <Avatar
+                sx={{
+                  height: 70,
+                  width: 70,
+                  m: 1,
+                  mt: "35%",
+                  bgcolor: "primary.main",
+                }}
               >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
+                <LockOutlinedIcon />
+              </Avatar>
+
+              <Typography colo="secondary" component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <Typography
+                  sx={{ left: 0, color: "white" }}
+                  component="h3"
+                  variant="h3"
+                >
+                  Inventory Management System
+                </Typography>
+              </Box>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                  sx={{ fontSize: "1.2rem" }}
+                  onChange={(e) => setUserName(e.target.value)}
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item></Grid>
                 </Grid>
-                <Grid item></Grid>
-              </Grid>
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
-    </ThemeProvider>
+      </div>
+      <div className="overlay3"></div>
+    </LoginWrapper>
   );
 }
 
